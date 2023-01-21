d66012ee4a532888e42393cdb63105e757fc5d89
Leonavshalom <leon.avshalom@gmail.com>
2023-01-21 19:29:07 +0200

    Add README

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import logging
 import requests
+import os
 logging.basicConfig(filename="functions.log", level=logging.DEBUG, filemode='a', format='%(asctime)s %(levelname)s: %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p')
 
@@ -51,7 +52,6 @@ def get_voice_input():
                 value = r.recognize_google(audio)
                 if len(value) != 0:
                     print("You said {}".format(value))
-                    break
                     return value
             except sr.UnknownValueError:
                 print("Oops! Didn't catch that")
@@ -62,24 +62,26 @@ def get_voice_input():
 
 
 def openai_request(text):
-    api_key = "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"
+    api_key = os.environ.get("OPENAI_API_KEY")
 
-    url = "https://api.openai.com/v1/engines/davinci/completions"
-
-    data = {
-        "prompt": text,
-        "temperature": 0.5,
-        "max_tokens": 100
-    }
+    url = "https://api.openai.com/v1/completions"
 
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}"
+        "Authorization": "Bearer {}".format(api_key)
     }
 
-    response = requests.post(url, json=data, headers=headers)
+    payload = {
+        "model": "text-davinci-003",
+        "prompt": "{}".format(text),
+        "temperature": 0,
+        "max_tokens": 100
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
 
     if response.status_code != 200:
         logging.debug(f"Error: {response.json()}")
+        print(f"Error: {response.json()}")
     else:
         return response.json()["choices"][0]["text"]
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import functions
 
-test = functions.mic_test()
-if test:
-    question = functions.get_voice_input()
-    answer = functions.openai_request(question)
+
+question = functions.get_voice_input()
+answer = functions.openai_request(question)
 
 print("The question is: \n", question)
 print("The answer is: \n", answer)
